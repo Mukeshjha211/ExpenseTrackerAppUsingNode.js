0c9fa9201da71ed9d2b16ed79edbe3c36c539c5a
Mukeshjha211 <114067075+Mukeshjha211@users.noreply.github.com>
2023-04-15 16:36:03 +0530

    Add files via upload

--- signupController.js
+++ signupController.js
@@ -1,21 +1,56 @@
 const Signup = require('../models/signup')
+const bcrypt = require('bcrypt')
+
+function isstringinvalid(string){
+    if(string ==undefined || string.length ===0){return true}
+    else{
+        return false
+    }
+}
 const SignupUsers = async(req,res)=>{
     try{
-        const name = req.body.name;
-        const email = req.body.email;
-        const password = req.body.password;
-    
-        const Data = await Signup.create({
-            name:name, email:email, password:password
-        }) 
-        res.status(201).json({signupDetails:Data})
+
+        const {name, email, password} = req.body;
+
+        if(isstringinvalid(name)||isstringinvalid(email)||isstringinvalid(password)){
+            return res.status(400).json({err:'Bad parameters, something id missing'})
+        }
+        const saltrounds = 10;
+        bcrypt.hash(password, saltrounds, async(err,hash)=>{
+            console.log(err)
+            await Signup.create({name,email,password:hash})
+            res.status(201).json({message:'Successfully create new user'})
+        })
         
         }catch(err){
-            res.status(500).json(err);
+            res.status(500).jsonf(err);
             console.log(err,"This is Post Error");
         }
 }
 
+const loginUsers = async(req,res)=>{
+    const {email, password} = req.body;
+    Signup.findAll({where:{email}}).then(user=>{
+        if(user.length>0){
+            bcrypt.compare(password, user[0].password, (err, result)=>{
+                if(err){
+                    res.status(200).json({success:false, message: 'Something went Wrong'})
+                }
+                if(result === true){
+                    res.status(200).json({success:true, message:"User logged in Successful"})
+                }
+                else{
+                    return res.status(400).json({success:false, message:'Password is Incorrect'})
+                }
+            })
+           
+        }
+    }).catch(err=>{
+        res.status(500).json({message:err, success:false})
+    })
+}
+
 module.exports = {
-    SignupUsers
+    SignupUsers,
+    loginUsers
 }
